d0465bf62dbadc76c7c2d426143ac48aa15410ae
maxiummax <max.jakkapat@gmail.com>
2018-04-02 15:59:51 +0700

    add layout
    update floating

---
+++ js/mouse.js
@@ -0,0 +1,12 @@
+                function onOver(obj){
+	   document.getElementById(obj).style.backgroundColor="#1f2023";
+       document.getElementById(obj).style.color="Darkgray"; 
+    }
+
+
+    function onOut(obj){
+        
+	   document.getElementById(obj).style.backgroundColor="#222427";
+    document.getElementById(obj).style.color= "white";
+        
+    }
